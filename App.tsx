d90c0ea04e988ce6b4e032163273c1d6e71135c3
Aman <bhaskarshah4235@gmail.com>
2025-08-30 13:15:55 +0530

    Resolve merge conflicts in App.tsx - keep local changes with proper navigation setup

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-<<<<<<< HEAD
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { enableScreens } from "react-native-screens";
 
@@ -12,13 +11,6 @@ enableScreens();
 import SignupScreen from "./Screens/SignupScreen";
 import LoginScreen from "./Screens/LoginScreen";
 import HomeScreen from "./Screens/HomeScreen";
-=======
-
-// Import screens
-import SignupScreen from "../screens/SignupScreen";
-import LoginScreen from "../screens/LoginScreen";
-import HomeScreen from "../screens/HomeScreen";
->>>>>>> 6141e5b8799fe1e06e767b27bc10fb6f45c39e16
 
 export type RootStackParamList = {
   Signup: undefined;
@@ -30,7 +22,6 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-<<<<<<< HEAD
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Signup">
@@ -40,14 +31,5 @@ export default function App() {
         </Stack.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
-=======
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Signup">
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
->>>>>>> 6141e5b8799fe1e06e767b27bc10fb6f45c39e16
   );
 }
